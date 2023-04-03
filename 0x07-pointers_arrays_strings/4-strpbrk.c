8d0025ec1774f61f8c891d9c206548caba43373d
benjaminzakka <125729476+benjaminzakka@users.noreply.github.com>
2023-04-04 00:05:08 +0100

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,10 +1,10 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _strpbrk - searches a string for any of a set of bytes.
- * @s: first string.
+ * _strpbrk - this searches a string for any of a set of bytes.
+ * @s: the first string.
  * @accept: second string.
- * Return: a pointer to the byte in s that matches one of the
+ * Return: returns a pointer to the byte in s that matches one of the
  * bytes in accept, or NULL if no such byte is found.
  */
 char *_strpbrk(char *s, char *accept)
