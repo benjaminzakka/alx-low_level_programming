29b1d4b15459ba5f2ad14c54e3464d0d7c06b734
benjaminzakka <125729476+benjaminzakka@users.noreply.github.com>
2023-04-04 00:03:54 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,9 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _strspn - gets the length of a prefix substring.
- * @s: initial segment.
- * @accept: accepted bytes.
+ * @s: the initial segment.
+ * @accept: number of accepted bytes.
  * Return: the number of accepted bytes.
  */
 unsigned int _strspn(char *s, char *accept)
