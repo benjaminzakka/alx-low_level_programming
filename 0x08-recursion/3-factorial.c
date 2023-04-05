a1ac2e409d2b579a6d7813cbdfe44282efb2f536
benjaminzakka <125729476+benjaminzakka@users.noreply.github.com>
2023-04-05 23:14:49 +0100

    Commit 7

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,9 +1,9 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * factorial - returns the factorial of a given number.
+ * factorial - this returns the factorial of a given number.
  * @n: input number.
- * Return: factorial of the number.
+ * Return: returns the factorial of the number.
  */
 int factorial(int n)
 {
